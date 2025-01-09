b6986eb5e41d3eaa0aa4251b0565595164ce3549
harikrishnaa5 <harikrishnaswathi5@gmail.com>
2025-01-09 16:23:30 +0530

    GET request and displaying fetched data on UI

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { Task } from '../Model/Task';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { CreateTaskComponent } from "./create-task/create-task.component";
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css'],
 })
-export class DashboardComponent{
+export class DashboardComponent implements OnInit{
   showCreateTaskForm: boolean = false;
+
+  allTasks : Task[] = []
 
   http: HttpClient = inject(HttpClient)
 
@@ -21,11 +23,37 @@ export class DashboardComponent{
     this.showCreateTaskForm = false;
   }
 
+  ngOnInit(): void {
+    this.FetchAllTasks()
+  }
+
+  FetchAllTasksClicked() {
+    this.FetchAllTasks()
+  }
+
   CreateTask(data: Task){
     const headers = new HttpHeaders({"my-header": "my-header"})
     this.http.post("https://angularhttpclient-b8ed3-default-rtdb.firebaseio.com/tasks.json", data, {headers: headers})
     .subscribe((res) => {
       console.log(res)
+      // this.FetchAllTasks()
+    })
+  }
+
+  private FetchAllTasks(){
+    this.http.get<{[key: string]: Task}>("https://angularhttpclient-b8ed3-default-rtdb.firebaseio.com/tasks.json")
+    .pipe(map((response) => {
+      console.log(response, 'response')
+      let tasks = []
+      for(let key in response) {
+        if(response.hasOwnProperty(key))
+        tasks.push({id: key, ...response[key]})
+      }
+      return tasks
+    }))
+    .subscribe((tasks) => {
+      console.log(tasks)
+      this.allTasks = tasks
     })
   }
 }
